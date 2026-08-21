189605f5caa9184ed584b5e40668a66dfa92f257
Abhinav Tarigoppula <abhinaaavvv07187@gmail.com>
2026-08-21 12:33:16 +0530

    Update open source stats to 20 merges across 12 organisations

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -270,7 +270,7 @@ export default function Landing() {
       {/* Right stats */}
       <div ref={rightRef} className="landing-right">
         {[
-          { num: '18', label: 'Merged PRs' },
+          { num: '20', label: 'Merged PRs' },
           { num: '212', label: 'LeetCode Solved' },
           { num: '+12%', label: 'ML Accuracy Gain' },
         ].map(s => (
